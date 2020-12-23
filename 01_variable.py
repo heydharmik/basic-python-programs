8297593b28f435788eaa4029c37d2bbc8d8fb387
Dharmik <dharmikpajwanigithub@gmail.com>
2020-12-23 08:23:53 +0530

    Printed the type of values

--- 01_variable.py
+++ 01_variable.py
@@ -8,7 +8,15 @@ a = "Dharmik"
 b = 67
 c = 23.243
 d = True
+
+# printing the variable
 print(a)
 print(b)
 print(c)
 print(d)
+
+# printing the type of variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
